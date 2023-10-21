16d72cacb4b458b68359ac703837975811852c06
AEKDA <k.david.doc@gmail.com>
2023-10-21 19:14:25 +0300

    create base servlet

--- src/main/java/itmo/web/lab2/AreaCheckServlet.java
+++ src/main/java/itmo/web/lab2/AreaCheckServlet.java
@@ -1,5 +1,30 @@
 package itmo.web.lab2;
 
-public class AreaCheckServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "area-check-servlet", value = "/area-check-servlet")
+public class AreaCheckServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        PrintWriter writer = resp.getWriter();
+
+        try {
+        float x = Float.parseFloat(req.getParameter("x-value"));
+        float y = Float.parseFloat(req.getParameter("y-value"));
+        float r = Float.parseFloat(req.getParameter("r-value"));
+        } catch(NullPointerException e) {
+            
+        }
+
+    }
 
 }
---
+++ src/main/java/itmo/web/lab2/ClearServlet.java
@@ -0,0 +1,15 @@
+package itmo.web.lab2;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ClearServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+    }
+
+}
